0554d57e76ca0d9e724db2cca9a466f93c3f0cb2
ZayyanMustafa <155172852+ZayyanMustafa@users.noreply.github.com>
2024-04-24 19:05:16 +0500

    Adding some function

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// Showing Alert and Prompt funcation including variables
+
+let message = "Its your warm welcome";
+var userName = prompt("Enter your name")
+alert(message  +" "+ userName + '!'  );
+
+//  Changing to uper case
+userName= userName.toUpperCase();
+document.write("Helow" +" "+ userName + '!' + " Welcome to the coding adventure with JavaScript.Let's dive in and start creating amazing things together!")
+
+// using if else if condition 
+
+var login =  userName
+if(userName == login)
+{
+    document.write("\n Thank you for submitting your fees. Your payment has been received and processed successfully.")
+} else{
+    document.write("\n Oops! Looks like we haven't received your fees yet. No worries, just a gentle reminder to settle up when you get a chance. Let's make sure everything's squared away!")
+}
