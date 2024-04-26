c20bc85113b6a87678fdc00a1ca302717e0816e9
ZayyanMustafa <155172852+ZayyanMustafa@users.noreply.github.com>
2024-04-26 15:20:45 +0500

    adding loop

--- script.js
+++ script.js
@@ -5,16 +5,22 @@ let message = "Its your warm welcome";
 var userName = prompt("Enter your name")
 alert(message  +" "+ userName + '!'  );
 
+
 //  Changing to uper case
 userName= userName.toUpperCase();
 document.write("Helow" +" "+ userName + '!' + " Welcome to the coding adventure with JavaScript.Let's dive in and start creating amazing things together!")
 
-// using if else if condition 
+// using if else condition with for loop 
 
-var login =  userName
-if(userName == login)
-{
-    document.write("\n Thank you for submitting your fees. Your payment has been received and processed successfully.")
-} else{
-    document.write("\n Oops! Looks like we haven't received your fees yet. No worries, just a gentle reminder to settle up when you get a chance. Let's make sure everything's squared away!")
+var studentsName = ['Zayyan','Mustafa','Hasan','Raza'] 
+for (var i = 0; i < studentsName.length; i++) {
+    if(userName == studentsName)
+    {
+        document.write("\n Thank you for submitting your fees. Your payment has been received and processed successfully.")
+    
+    } else{
+        document.write("\n Oops! Looks like we haven't received your fees yet. No worries, just a gentle reminder to settle up when you get a chance. Let's make sure everything's squared away!")
+    }
 }
+
+
