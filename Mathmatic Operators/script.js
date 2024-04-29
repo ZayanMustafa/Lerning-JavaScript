235ba75c353cd19e7191a673c61bdc17b2e2a469
ZayyanMustafa <160398419+ZayyanMustafa13@users.noreply.github.com>
2024-04-29 13:21:35 +0500

    Making a Calculator

---
+++ Mathmatic Operators/script.js
@@ -0,0 +1,11 @@
+
+// Function to add a digit to the input bar
+function Multipliit( Number){
+    // Get the input bar element by its ID
+    var inputBar = document.getElementById("inputBar");
+    // digit to the input bar value
+    inputBar.value += Number;
+}
+var num01 = document.getElementById(inputBar) 
+ 
+
