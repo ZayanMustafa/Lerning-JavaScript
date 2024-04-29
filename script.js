15f37369c4a0f4532d6960e960b1a311008d142b
ZayyanMustafa <155172852+ZayyanMustafa@users.noreply.github.com>
2024-04-29 07:52:47 +0500

    update script.js

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ var userName = prompt("Enter your name")
 alert(message  +" "+ userName + '!'  );
 
 
-//  Changing to uper case
-userName= userName.toUpperCase();
-document.write("Helow" +" "+ userName + '!' + " Welcome to the coding adventure with JavaScript.Let's dive in and start creating amazing things together!")
-
 // using if else condition with for loop 
 
 var studentsName = ['Zayyan','Mustafa','Hasan','Raza'] 
@@ -17,10 +13,13 @@ for (var i = 0; i < studentsName.length; i++) {
     if(userName == studentsName)
     {
         document.write("\n Thank you for submitting your fees. Your payment has been received and processed successfully.")
-    
+        
     } else{
         document.write("\n Oops! Looks like we haven't received your fees yet. No worries, just a gentle reminder to settle up when you get a chance. Let's make sure everything's squared away!")
     }
 }
 
+//  Changing to uper case
+userName= userName.toUpperCase();
+document.write("Helow" +" "+ userName + '!' + " Welcome to the coding adventure with JavaScript.Let's dive in and start creating amazing things together!")
 
