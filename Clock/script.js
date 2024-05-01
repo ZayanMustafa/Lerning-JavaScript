8d1029ef760369b53793188275dfc493cb3fc0d4
ZayyanMustafa <160398419+ZayyanMustafa13@users.noreply.github.com>
2024-05-01 16:38:39 +0500

    just complete js
    Completed JavaScript , but facing problem on adding these elements on html
    
    Co-Authored-By: ZayyanMustafa <155172852+ZayyanMustafa@users.noreply.github.com>

--- Clock/script.js
+++ Clock/script.js
@@ -1,15 +1,55 @@
-function updateTime() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    const timeString = `${hours}:${minutes}:${seconds}`;
-    document.getElementById('time').textContent = timeString;
-  }
-  
-  // Update time every second
-  setInterval(updateTime, 1000);
-  
-  // Initial call to display time immediately
-  updateTime();
-  
+// Make object hear , you can edit anything in object 
+// by using var name (in below example object) with propertie name
+// like :
+
+// var object = {
+//   name : "Hasan" ,
+//   age : 17  ,
+//   profesion : "Student" ,
+// }
+// console.log(object)
+
+// edit name propertie
+// object.name = "Zayyan" 
+// console.log(object)
+
+// edit age propertie
+// object.age = 17.6 
+// console.log(object)
+
+// edit profession propertie
+// object.profession = 'Web devolper'
+// console.log(object)
+
+// Date is used for  "Date and Time as Country" 
+var few = new(Date)
+console.log(few)
+
+// toLocaleDateString() converts a date into simple date (Month,Date,Year)
+var dateString = few.toLocaleDateString();
+console.log(dateString);
+
+// Day Names : 
+// var dayNames = ['Monday' , 'Tuesday' , 'Wednesday' , 'Thursday' ,'Friday' , 'Saturday' , 'Sunday']
+// console.log(dayNames.toString(" "))
+
+// For getting  today's day :
+// console.log( 'Day name :', dayNames[few.getDay()])
+
+// Get Hours Mints Second from var = "few"
+console.log(few.getDate())
+console.log(few.getMonth())
+console.log(few.getFullYear())
+
+// For Timing : 
+console.log(few.getHours())
+console.log(few.getMinutes())
+console.log(few.getSeconds())
+console.log(few.getMilliseconds())
+
+// For Day's :
+console.log(few.getDay())
+
+var hours = document.getElementById('hours')
+var minutes = document.getElementById('minutes')
+var seconds = document.getElementById('seconds')
