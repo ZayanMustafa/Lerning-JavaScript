d4968f92f699ceaea62bb1428acbdf60c59374cb
ZayyanMustafa <160398419+ZayyanMustafa13@users.noreply.github.com>
2024-05-05 12:17:54 +0500

    Update.js

--- counter/script.js
+++ counter/script.js
@@ -2,16 +2,17 @@ function sumbitBtn(){
     var date = document.getElementById("Date").value;
     var month = document.getElementById("Month").value;
     var year = document.getElementById("Year").value;
-     console.log("User input:", date +"/"+ month +"/"+ year);
-   
+     console.log("User input---->", date +"/"+ month +"/"+ year);
+    
     // Bhai Sunooo yar! 
-    var hmm = document.getElementById("hm").innerText
-    console.log(hmm)
-    hmm = "2 mint rukoo!";
-    console.log(hmm)
-
+    // var hmm = document.getElementById("hm").innerText
+    // console.log(hmm)
+    // hmm = "2 mint rukoo!";
+    // console.log(hmm)
 }
 
+var hasan = document.getElementById("Month").innerText
+console.log(hasan)
 
 
 
