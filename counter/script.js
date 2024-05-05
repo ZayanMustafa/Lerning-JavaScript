684383f03c065d3ee0d18ca351a4916b27097c21
ZayyanMustafa <160398419+ZayyanMustafa13@users.noreply.github.com>
2024-05-05 17:07:44 +0500

    just complete counter

--- counter/script.js
+++ counter/script.js
@@ -1,23 +1,65 @@
-function sumbitBtn(){
-    var date = document.getElementById("Date").value;
-    var month = document.getElementById("Month").value;
-    var year = document.getElementById("Year").value;
-     console.log("User input---->", date +"/"+ month +"/"+ year);
+document.addEventListener('DOMContentLoaded', function() {
+    var input1 = document.getElementById('Month');
+    var input2 = document.getElementById('Day');
+    var input3 = document.getElementById('Year');
     
-    // Bhai Sunooo yar! 
-    // var hmm = document.getElementById("hm").innerText
-    // console.log(hmm)
-    // hmm = "2 mint rukoo!";
-    // console.log(hmm)
-}
-
-var hasan = document.getElementById("Month").innerText
-console.log(hasan)
 
 
+    input1.addEventListener('keydown', function(event) {
+    //   if (input1 > 12) {alert("Enter Correct Month ")}
+        if (event.key === 'Enter') {
+        event.preventDefault();
+        input2.focus(); 
+      }
+    });
+  
+    input2.addEventListener('keydown', function(event) {
+    //   if (input2 > 31){alert("Enter Correct Date ")}
+      if (event.key === 'Enter') {
+        event.preventDefault(); 
+        input3.focus(); 
+      }
+    });
 
+    input3.addEventListener('keydown', function(event) {
+    //   if (input2 < 2023){alert("Enter Correct Year ")}
+        if (event.key === 'Enter') {
+          event.preventDefault(); 
+          btn.focus(); 
+      }
+      });
 
+    btn.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+          event.preventDefault(); 
+        }
+      });
 
+      
+  });
+  function hasan() {
+    var userInput = [document.getElementById('Month').value, document.getElementById('Day').value, document.getElementById('Year').value]
+    var targetDate = new Date(userInput);
+    var countdownInterval = setInterval(function() {
+    var currentDate = new Date();
+    var timeDifference = targetDate.getTime() - currentDate.getTime();
 
+    var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
+    document.getElementById('counter').innerHTML = 'Time remaining: ' + days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
 
+    if (timeDifference < 0) {
+      clearInterval(countdownInterval);
+      document.getElementById('counter').innerHTML = 'This date is gone....';
+    }
+    if (userInput == null ){
+        alert("Enter Value!")
+    }
+  }, 1000);
+
+  }
+
+  
