1fbe101679308c2b2a357757040cd16921b03e9f
Zayyan Mustafa <160398419+ZayanMustafa@users.noreply.github.com>
2024-05-26 14:42:47 +0500

    Want to update

--- Very basic of js/script.js
+++ Very basic of js/script.js
@@ -6,9 +6,6 @@ var userName = prompt("Enter your name")
 alert(message  +" "+ userName + '!'  );
 
 //  Changing to uper case
-userNameTop= userName.toUpperCase();
-document.write("Helow" +" "+ userNameTop + '!' + " Welcome to the coding adventure with JavaScript.Let's dive in and start creating amazing things together!")
-
 // using if else condition with for loop
 
 var userData = ['Zayyan','Mustafa','Hasan','Raza','Hussain','Ahmed']
@@ -16,12 +13,17 @@ let checkingCorner = false;
 for (let i = 0; i < userData.length; i++) {
     if (userName === userData[i]) {
     checkingCorner = true;
-        break;
-    }
+        console.log(checkingCorner)
+    break;
 }
+}
+userNameTop= userName.toUpperCase();
+document.write("Helow" +" "+ userNameTop + '!' + " Welcome to the coding adventure with JavaScript.Let's dive in and start creating amazing things together!")
+
 if (checkingCorner) {
-document.write("<br>You win");
+document.write("<br> <h1>You win </h1>");
 } else {
-document.write("<br>Try Again!");
+document.write("<br> <h1>Try Again!</h1>");
+
 }
 
