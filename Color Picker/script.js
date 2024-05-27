bc326205c34780a179cf2986d65220579d4d09db
Zayyan Mustafa <160398419+ZayanMustafa@users.noreply.github.com>
2024-05-27 16:05:04 +0500

    Update

---
+++ Color Picker/script.js
@@ -0,0 +1,8 @@
+var representative = document.getElementById('representative')
+var box1 = document.getElementById('box1')
+var box2 = document.getElementById('box2')
+var box3 = document.getElementById('box3')
+var box4 = document.getElementById('box4')
+var box5 = document.getElementById('box5')
+
+box1.style.background = "Red"
