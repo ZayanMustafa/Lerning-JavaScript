6213d8612186ecce1c0c5c8fb7a2d5947c16d7f3
Zayyan Mustafa <160398419+ZayanMustafa@users.noreply.github.com>
2024-05-31 10:36:08 +0500

    Color Picker Completd!

--- Color Picker/script.js
+++ Color Picker/script.js
@@ -1,8 +1,36 @@
-var representative = document.getElementById('representative')
-var box1 = document.getElementById('box1')
-var box2 = document.getElementById('box2')
-var box3 = document.getElementById('box3')
-var box4 = document.getElementById('box4')
-var box5 = document.getElementById('box5')
+var show = document.getElementById("show")
+var box1 = document.getElementById("box1")
+var box2 = document.getElementById("box2")
+var box3 = document.getElementById("box3")
+var box4 = document.getElementById("box4")
+var box5 = document.getElementById("box5")
 
-box1.style.background = "Red"
+function forbody(){
+    show.style.backgroundColor = "white"
+    var text = "Ready to paint your digital canvas? Select your hue!"
+    // text.style.color = "Black"
+    show.innerHTML = text
+
+}
+    // Funcations for Changing the Color 
+function first(){
+    show.style.backgroundColor = box1.style.backgroundColor
+    show.innerHTML = ""
+}
+function second(){
+    show.style.backgroundColor = box2.style.backgroundColor
+    show.innerHTML = ""
+}
+function third(){
+    show.style.backgroundColor = box3.style.backgroundColor
+    show.innerHTML = ""
+}
+function fourth(){
+    show.style.backgroundColor = box4.style.backgroundColor
+    show.innerHTML = ""
+}
+function fifht(){
+    show.style.backgroundColor = box5.style.backgroundColor
+    show.innerHTML = ""
+}
+
