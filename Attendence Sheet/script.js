cfe4c73a3e23dedfeca49fdc8a48b52ea7658333
Zayyan Mustafa <160398419+ZayanMustafa@users.noreply.github.com>
2024-06-09 11:52:42 +0500

    update

--- Attendence Sheet/script.js
+++ Attendence Sheet/script.js
@@ -1,13 +1,6 @@
 var loginPage = document.getElementById("showcase");
 var classSheet = document.getElementById("classSheet");
 
-// function login() {
-//   alert("Sabar Kar Bhai");
-//   var page = document.getElementById("Attendance"). value
-//   console. log ("page") 
-//   page.style.display = 'block';
-// }
-
   function signIn(event) {
       event.preventDefault(); // Prevent form submission and page reload
       
@@ -21,19 +14,20 @@ var classSheet = document.getElementById("classSheet");
           alert("Please fill in both email and password fields.");
       } else if (!email.match(emailFormat)) {
           alert("Please enter a valid email address.");
-      } else {
-        
-    // alert("👋 Hey! Welcome back, Teacher!");
-    // After successful sign-in, hide the login page and display the next page
-    if (loginPage) {
-      loginPage.style.display = "none";
-        }
-        if (classSheet) {
-          classSheet.style.display = "block";
-          classSheet.style.overflowX = "hidden"
-
-        }
-  
+          } else {
+            
+            if (loginPage) {
+              loginPage.style.display = "none";
+              }
+              if (classSheet) {
+                classSheet.style.display = "block";
+                classSheet.style.overflowX = "hidden"
+                
+                }
+                var show = document.getElementById("showEmail");
+                show.innerText = email;
+                console.log(show);
+          
   }}
 
     // Attendence Sheet 
@@ -67,9 +61,8 @@ var classSheet = document.getElementById("classSheet");
               <td>${index + 1}</td>
               <td>${student.name}</td>
               <td>${student.fatherName}</td>
-            
-          </tr>`;
-      }).join('');
+           </tr>`;
+           }).join('');
   }
   
   updateTable();
