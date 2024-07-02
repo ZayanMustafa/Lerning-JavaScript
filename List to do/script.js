dcded982581200c7afd45e53fa6d9742ba28e7f4
Zayyan Mustafa <160398419+ZayanMustafa@users.noreply.github.com>
2024-07-02 11:39:47 +0500

    Updating

--- List to do/script.js
+++ List to do/script.js
@@ -4,20 +4,16 @@ document.getElementById("addTaskBtn").addEventListener("click", taskAddtoList)
 document.getElementById("findTaskbtn").addEventListener("change", findTask)
 
 function taskAddtoList(){
-
+    
     let userInput = inputBar.value
     console.log(userInput)
     userList.innerHTML +=    `  <li class="list-group-item mt-5 d-flex justify-content-between align-items-center">
-                ${userInput}
-                <button type="button" class="btn btn-outline-danger btn-sm ml-2">Remove</button>
-            </li>`
+    ${userInput}
+    <button type="button" id="removeTaskBtn" class="btn btn-outline-danger btn-sm ml-2">Remove</button>
+    </li>`
     inputBar.value = ""
+    document.getElementById("removeTaskBtn").addEventListener("click", removeTask)
 }
-
-
-
-
-
 
 
 
@@ -25,6 +21,9 @@ function findTask(){
     console.log(inputBar.value)
 }
 
-
+function removeTask (){
+    confirm("Are you suerly want to remove it")
+    // alert("ahaa")
+}
 
 
