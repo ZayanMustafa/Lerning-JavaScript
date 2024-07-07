235bb0884307491454e46870d83b84f495446321
Zayan Mustafa <mustafazayyan13@gmail.com>
2024-07-08 01:32:19 +0500

    Working

--- Authencation/signIn.js
+++ Authencation/signIn.js
@@ -1,1 +1,6 @@
 const userEmail = document.getElementById("userEmail")
+document.getElementById("logInBtn").addEventListener("click" , userSignIn)
+
+function userSignIn(){
+    alert("Busss boss")
+}
