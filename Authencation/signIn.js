0e8dafeda3e70c64e31ea8f36dbad8bcc07ddba0
Zayyan Mustafa <160398419+ZayanMustafa@users.noreply.github.com>
2024-07-08 12:51:34 +0500

    Updated

--- Authencation/signIn.js
+++ Authencation/signIn.js
@@ -8,14 +8,14 @@ signUpBtn.addEventListener("click", signIn);
 
 
 // Firebase Auth State Change Listener
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        // window.location.href = 'signInUserPage.html';
-        const uid = user.uid;
-    } else {
-        // window.location.href = 'index.html';
-    }
-});
+// onAuthStateChanged(auth, (user) => {
+//     if (user) {
+//          window.location.href = 'signInUserPage.html';
+//         const uid = user.uid;
+//     } else {
+//       window.location.href = 'index.html';
+//     }
+// });
 // Sign In Function 
 function signIn (){
   signInWithEmailAndPassword(auth, userEmail.value, userSignInPassword.value)
