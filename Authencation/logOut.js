0e75e7acd23d378ece0ee975a311a84d4a0119ff
Zayyan Mustafa <160398419+ZayanMustafa@users.noreply.github.com>
2024-07-08 13:19:32 +0500

    Updated

---
+++ Authencation/logOut.js
@@ -0,0 +1,4 @@
+
+const username = "User";
+document.querySelector('.logout-container').innerHTML += `
+      <p class="animate__animated animate__fadeInUp mt-2">See you next time, ${username} ! 👋</p>`;
