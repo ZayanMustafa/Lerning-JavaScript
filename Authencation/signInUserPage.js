f962f660d23720b9c496338b3340f454f51534a4
Zayyan Mustafa <160398419+ZayanMustafa@users.noreply.github.com>
2024-08-11 10:51:49 +0500

    Updated

--- Authencation/signInUserPage.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("logoutOption").addEventListener("click", logOutFunction);
-
-function logOutFunction() {
-    if (confirm('Are you sure you want to log out?')) {
-        window.location.href = 'logout.html';
-    }
-}
